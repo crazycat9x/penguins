b619e2e27710ff23e3df1867f93998c2abadd6b0
Derek Kim <wkim505@aucklanduni.ac.nz>
2018-08-18 14:07:50 +1200

    Search bar add eventListener

--- createBusCard.js
+++ createBusCard.js
@@ -1,43 +1,74 @@
 function createBusCard(busNumber, cityName, busStatus){
-  const wrapper = createHtmlElement({className: "bus-card"})
-  const number = createHtmlElement({className: "bus-number", content: busNumber})
-  const name = createHtmlElement({className: "city-name", content: cityName})
-  const deleteButton = createHtmlElement({type: "button", className: "delete-button"})
-  const status = createHtmlElement({className: "bus-status", content: busStatus})
-  wrapper.appendChild(number)
-  wrapper.appendChild(name)
-  wrapper.appendChild(deleteButton)
-  wrapper.appendChild(status)
-  return wrapper
+  const wrapper = createHtmlElement({className: "bus-card"});
+  const topSection = createHtmlElement({className: "top-bus-card"});
+  const bottomSection = createHtmlElement({className: "bottom-bus-card"});
+  const number = createHtmlElement({className: "bus-number", content: busNumber});
+  const name = createHtmlElement({className: "city-name", content: cityName});
+  const deleteButton = createHtmlElement({type: "button", className: "delete-button"});
+  deleteButton.addEventListener("click", ()=> wrapper.parentNode.removeChild(wrapper)); //send to backend
+  const status = createHtmlElement({className: "bus-status", content: busStatus});
+  topSection.appendChild(number);
+  topSection.appendChild(deleteButton);
+  bottomSection.appendChild(name);
+  bottomSection.appendChild(status);
+  wrapper.appendChild(topSection);
+  wrapper.appendChild(bottomSection);
+  return wrapper;
 }
 
 function renderBusListToPage(page){
-  page.appendChild(createHtmlElement({type: "input", className: "add-bar", additionalAttr:{"placeholder": "Add a route"}}))
+  const routeDummyData = {
+    routeNumbers : [
+      {
+        "number": "267"
+      },
+      {
+        "number":"366"
+      },
+      {
+        "number":"565"
+      },
+      {
+        "number":"570"
+      },
+      {
+        "number":"572"
+      }
+    ]
+  }
   const dummyData = {
     "routes" : [
-    {
-      "number": "667",
-      "name": "Henderson",
-      "status": "ONTIME"
-    },
-    {
-      "number": "111",
-      "name": "City Central",
-      "status": "ONTIME"
-    },
-    {
-      "number": "222",
-      "name": "Mt Eden",
-      "status": "Cancelled"
-    }
-  ]
+      {
+        "number": "667",
+        "name": "Henderson",
+        "status": "ONTIME"
+      },
+      {
+        "number": "111",
+        "name": "City Central",
+        "status": "ONTIME"
+      },
+      {
+        "number": "222",
+        "name": "Mt Eden",
+        "status": "Cancelled"
+      }
+    ]
   }
+  const inputForm = createHtmlElement({type: "input", className: "add-bar", additionalAttr:{"placeholder": "Add a route"}});
+  page.appendChild(inputForm);
+
+  inputForm.addEventListener("keydown", function(e){
+    if (!e) { const e = window.event;}
+    if (e.keyCode == 13) { openModalWithData(this.value); }
+  }, false); //send to backend and display the result
+
   for (const route of dummyData.routes){
-    console.log(route)
-    console.log(route.number, route.name, route.status)
-    const card = createBusCard(route.number, route.name, route.status) //declare outside loop?
-    console.log(card)
-    page.appendChild(card)
+    console.log(route);
+    console.log(route.number, route.name, route.status);
+    const card = createBusCard(route.number, route.name, route.status); //declare outside loop?
+    console.log(card);
+    page.appendChild(card);
   }
   // reqwest("GET","url")
   // .then(res => JSON.parse(res))
@@ -48,5 +79,5 @@ function renderBusListToPage(page){
   //     page.appendChild(card)
   //   }
   // })
-  return page
+  return page;
 }
